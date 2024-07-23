cf1d7df793513f4f51321c2dc06ae6156727f3dd
Adit Rajaram <layarajaram@aditrajaram-aditsmbp.home>
2024-07-23 08:31:54 -0500

    Init Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        //Declare Control Variable
+        int i = 0;
+        while(i<10) {
+            System.out.println(i);
+            i += 1;
+        }
+    }
+}
